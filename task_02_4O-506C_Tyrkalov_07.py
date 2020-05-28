1a34eac4042f29a6d754d0d2fb176c959d31685e
Никита Т <65958048+tn018@users.noreply.github.com>
2020-05-28 13:20:39 +0300

    Update task_02_4O-506C_Tyrkalov_07.py

--- task_02_4O-506C_Tyrkalov_07.py
+++ task_02_4O-506C_Tyrkalov_07.py
@@ -5,40 +5,36 @@ import re
 import scipy.special as sc
 import urllib.request as ur
 
+def h(n, x):
+    return sc.spherical_jn(n, x) + 1j * sc.spherical_yn(n, x)
+
 def a(n, x):
-    return sc.spherical_jn(n, x) / h(n, x) 
+    return sc.spherical_jn(n, x) / h(n, x)
 
 def b(n, x):
     return ((x * sc.spherical_jn(n - 1, x) - n * sc.spherical_jn(n, x))
             / (x * h(n - 1, x) - n * h(n, x)))
-    
-def h(n, x):
-    return sc.spherical_jn(n, x) + 1j * sc.spherical_yn(n, x)
 
 def RCS(D, fmin, fmax):
     c = 3e8
-    r = D/2
+    r = float(D/2)
     f = np.arange(fmin, fmax+1e6, 1e6)
-    sigma, F = [], []
+    sigma = []
     for i in f:
         lambda_ = c / i
         k = 2 * np.pi / lambda_
         summa = []
-        for n in range(1,110):
-            summa.append((-1) ** n * (n + 0.5) * (b(n, k * r) - a(n, k * r)))
+        for n in range(1,100):
+            summa.append( (-1) ** n * (n + 0.5) * (b(n, k * r) - a(n, k * r)) )
         sigma.append((lambda_ ** 2 / np.pi) * abs(sum(summa)) ** 2)
-        F.append(i)
-    w = open('results/task_02_4О-506C_Tyrkalov_07.txt', 'w')
-    print ('f, [МГц]\tsigma, [м^2]\n', file = w)
-    d = dict(zip(F, sigma))
-    for key, value in d.items():
-        print('{0}\t\t{1}\n'.format(key * 1e-6, value), file = w)
-    w.close()
-    
+    with open('results/task_02_4O-506C_Tyrkalov_07.txt', 'w') as ouf:
+        ouf.write('f, [ГГц]\tsigma, [м^2]\n')
+        for i in range(len(f)):
+            ouf.write(str('{0:.4f}\t\t{1}\n'.format(f[i] * 1e-9, sigma[i])))
     plt.plot(f, sigma)
     plt.grid()
     plt.ylabel(r'$\sigma$, [$м^2$]', fontsize = 18)
-    plt.xlabel('$f$, [Гц]')
+    plt.xlabel('$f$, [ГГц]')
     plt.show()
            
 if __name__ == '__main__':
@@ -49,13 +45,13 @@ if __name__ == '__main__':
     for i in r.readlines():
         С = str(i.strip())
         lines.append(С[2:-1])
-    for i in lines:
-        if re.match(r'7\.', i): z= i
+        
+    Nomer_varianta = 7
+    z = lines[Nomer_varianta - 1]
+    print(z)
+    
     z1 = (z.split(';'))
     A = [z1[0].split('=')[1], z1[1].split('=')[1], z1[2].split('=')[1]]
     B = [float(x) for x in A]
     D, fmin, fmax = B[0], B[1], B[2]
-    print(D,type(D))
-    print(fmin,type(fmin))
-    print(fmax,type(fmax))
     RCS(D, fmin, fmax)
